4611cf5ea3d6d88227168385b319f327b99fe9ad
rainyjune <rainyjune@live.cn>
2015-04-26 14:58:20 +0800

    Add #cssBtn

--- helper.js
+++ helper.js
@@ -12,8 +12,20 @@ if (sortTable) {
   jsNdbBtn.type = "checkbox";
   jsNdbBtn.title = "选择所有非Debug JS文件";
   
+  var cssLabel = document.createElement("label");
+  cssLabel.id = "cssLabel";
+  cssLabel.htmlFor = "cssBtn";
+  cssLabel.innerHTML = "所有CSS文件";
+  var cssBtn = document.createElement("input");
+  cssBtn.id = "cssBtn";
+  cssBtn.type = "checkbox";
+  cssBtn.title = "选择所有CSS文件";
+  
   helperDiv.appendChild(jsNdbLabel);
   helperDiv.appendChild(jsNdbBtn);
+  
+  helperDiv.appendChild(cssLabel);
+  helperDiv.appendChild(cssBtn);
   
   sortTable.insertAdjacentHTML("beforeBegin", helperDiv.outerHTML);
   
@@ -26,6 +38,9 @@ if (sortTable) {
         case "jsNdbBtn":
           toggleJSSelection();
           break;
+        case "cssBtn":
+          toggleCSSSelection();
+          break;
       }
     }
   });
@@ -34,6 +49,15 @@ if (sortTable) {
     var jsSelectBtn = document.querySelector("#jsNdbBtn");
     var isChecked = jsSelectBtn.checked;
     var fileList = sortTable.querySelectorAll("input[value$='.js']:not([value$='-debug.js'])");
+    for (var i = 0, len = fileList.length; i < len; i++) {
+      fileList[i].checked = isChecked;
+    }
+  }
+  
+  function toggleCSSSelection() {
+    var cssSelectBtn = document.querySelector("#cssBtn");
+    var isChecked = cssSelectBtn.checked;
+    var fileList = sortTable.querySelectorAll("input[value$='.css']");
     for (var i = 0, len = fileList.length; i < len; i++) {
       fileList[i].checked = isChecked;
     }
